f5ed99bb38728421fe32f48225519e56c7464175
Reeeeeeeen3 <38648933+Reeeeeeeen3@users.noreply.github.com>
2018-11-16 19:27:42 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,19 @@
-/*ダイアログを表示*/
-window.alert('こんにちは世界！');
-//コンソールに表示
-console.log('こんにちは！');
-//日時を表示
-document.getElementById('choice').textContent = new Date();
+//答えを用意
+var number = Math.floor(Math.random()*11);
+var answer = parseInt(window.prompt('数あてゲーム。0~10の数字を入力してください！'));
+
+var message;
+if(answer === number) {
+  message = 'あたり！';
+  }else if (answer < number){
+    message = '残念！もっと大きいです';
+  }else if (answer > number)
+    message = '残念！もっと小さいです';
+  }else{
+    message = '0~10の数字を入力してください'；
+  }
+
+//結果を表示
+document.getElementById('choice').textContent = message;
+
+  
