7ed0e9c9df653ffc5c42d030310fa4c067b0ebb4
Reeeeeeeen3 <38648933+Reeeeeeeen3@users.noreply.github.com>
2018-11-16 19:31:14 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -8,9 +8,9 @@ if(answer === number) {
   }else if (answer < number){
     message = '残念！もっと大きいです';
   }else if (answer > number)
-    message = '残念！もっと小さいです';
+    message = '残念！もっと小さいです'
   }else{
-    message = '0~10の数字を入力してください'；
+    message = '0~10の数字を入力してください';
   }
 
 //結果を表示
