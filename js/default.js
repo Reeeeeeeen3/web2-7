20b5b07582940ce4faa464ab458e445c6ee55a85
Reeeeeeeen3 <38648933+Reeeeeeeen3@users.noreply.github.com>
2018-11-19 16:08:15 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,14 +1,15 @@
 //答えを用意
 var number = Math.floor(Math.random()*11);
 
-var i = 1;
+var i = 0;
+
 do{
 //ダイアログ表示
 var answer = parseInt(window.prompt('数あてゲーム。0~10の数字を半角で入力してください！'));
 var message;
 if(answer === number) {
   message = 'あたり！';
-  i = 2;
+  i++;
   }else if (answer < number){
     message = '残念！もっと大きいです';
   }else if (answer > number){
@@ -20,4 +21,6 @@ if(answer === number) {
 
 //結果を表示
 document.getElementById('choice').textContent = message;
-}while(i === 2);
+}while(i == 1);
+
+return i;
